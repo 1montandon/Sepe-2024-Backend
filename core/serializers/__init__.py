1d88688c3d9d15913279877315a01972a02e81c6
ruantoddynho <ruanvictorfelicio01@gmail.com>
2024-08-20 15:06:26 -0300

    feat jogador 80%

--- core/serializers/__init__.py
+++ core/serializers/__init__.py
@@ -2,4 +2,5 @@ from .user import UserSerializer
 from .campeonato import CampeonatoWriteSerializer, CampeonatoDetailSerializer
 from .time import TimeWriteSerializer, TimeDetailSerializer
 from .rodada import RodadaDetailSerializer, RodadaWriteSerializer
+from .jogador import JogadorDetailSerializer, JogadorWriteSerializer
 
--- core/views/__init__.py
+++ core/views/__init__.py
@@ -2,3 +2,4 @@ from .user import UserViewSet
 from .campeonato import CampeonatoViewSet
 from .time import TimeViewSet
 from .rodada import RodadaViewSet
+from .jogador import JogadorViewSet
