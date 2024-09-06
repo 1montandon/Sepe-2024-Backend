0a24a8f5af5bff27d520cd7b029d777b9f7bc25a
1montandon <emontandon11@gmail.com>
2024-09-06 08:54:25 -0300

    Upload e associação de imagens

--- core/serializers/time.py
+++ core/serializers/time.py
@@ -1,30 +1,29 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
+
+from uploader.models import Image
+from uploader.serializers import ImageSerializer
+
 from core.models import Time
 
+
 class TimeDetailSerializer(ModelSerializer):
+    escudo = ImageSerializer(required=False)
+    
     class Meta:
         model = Time
-        fields: list[str] = [
-            "id",
-            "nome",
-            "gols_pro",
-            "gols_contra",
-            "vitoria",
-            "derrota",
-            "pontos",
-            "campeonato"
-        ]
-          
+        fields: list[str] = ["id", "nome", "gols_pro", "gols_contra", "vitoria", "derrota", "pontos", "campeonato", "escudo"]
+
 
 class TimeWriteSerializer(ModelSerializer):
+    escudo_attachment_key = SlugRelatedField(
+        source="escudo",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    escudo = ImageSerializer(required=False, read_only=True)
+    
     class Meta:
         model = Time
-        fields: list[str] = [
-            "nome",
-            "gols_pro",
-            "gols_contra",
-            "vitoria",
-            "derrota",
-            "pontos",
-            "campeonato"
-        ]
+        fields: list[str] = ["nome", "gols_pro", "gols_contra", "vitoria", "derrota", "pontos", "campeonato", "escudo_attachment_key", "escudo"]
