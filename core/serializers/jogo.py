786153aa78af7d31c35d4a3f31d6efc10e3c1030
1montandon <emontandon11@gmail.com>
2024-10-04 08:28:24 -0300

    time mandante e visitante

--- core/serializers/jogo.py
+++ core/serializers/jogo.py
@@ -1,7 +1,9 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 from core.models import Jogo
 
 class JogoDetailSerializer(ModelSerializer):
+    time_mandante = CharField(source='time_mandante.nome')
+    time_visitante = CharField(source='time_visitante.nome')
     class Meta:
         model = Jogo
         fields: list[str] = [
